0bda85d0bb02a37e1174228531a768f039d206df
Guillermo <wolf.fox1985@gmail.com>
2021-03-06 00:45:10 +0100

    Fixed dependencies.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/ryanfox1985/OctoPrint-Youtube-Timelapse"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = ['google_auth_oauthlib']
+plugin_requires = ['oauth2client', 'google-api-python-client']
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
